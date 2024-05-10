44c84b9b4513ed8ec7f960551538b42fa1cffec2
KyungChan <philip9504@naver.com>
2024-05-10 14:17:52 +0900

    실전 앱 만들기 4-1. 버튼을 눌렀을 때 반응하는 기능
    activity_main.xml에서 button 생성 후, 선 연결로 버튼 위치 고정.
    MainActivity.kt에서 button 클릭시 Toast 메세지 생성되도록 코드 입력.
    
    Toast 사용시 Toast에서 alt+enter를 통해 toast widget을 import 할 수 있으나,
    button의 경우 책에서 제시된 kotlin-android-extensions가 deprecate 되면서,
    다른 방법을 사용해야 함.
    
    build.gradle(Module)의 android에서 view Binding 기능 추가.
    Activity_main.xml의 바인딩 클래스 ActivtyMainBinding을 이용하여,
    mBinding 생성 후, inflate 및 setContentView(mBinding.root)로 뷰 바인딩.
    MainActivity에서 activity_main의 id가 button_4_1인 버튼을 가져와,
    button41 = mBinding.button41; // _언더바가 생략됨.
    로 하여금 button41로 클릭 이벤트 함수 setOnClickListener를 작성.
    setOnClickListener 함수 안에 Toast 메시지를 띄우는 함수 Toast.makeText를 작성하여 4-1 완료.

--- app/src/main/java/com/example/kotlin_ex/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_ex/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.example.kotlin_ex
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.kotlin_ex.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // 뷰 바인딩
+    private lateinit var mBinding : ActivityMainBinding;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        
+        //setContentView(R.layout.activity_main) 대신 뷰 바인딩 사용
+        // ActivityMainBinding 으로 뷰 바인딩
+        mBinding = ActivityMainBinding.inflate(layoutInflater);
+        setContentView(mBinding.root);
 
+        var button41 = mBinding.button41;
+
+        // button id : button_4_1
+        button41.setOnClickListener {
+            Toast.makeText(this@MainActivity,
+                "님들 토스토 보임?",
+                Toast.LENGTH_LONG).show();
+        }
     }
 }
