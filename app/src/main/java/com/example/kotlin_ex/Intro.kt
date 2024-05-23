7a8c684598e450dc7ae8d7afb6418d6a2525a133
KyungChan <philip9504@naver.com>
2024-05-23 14:34:37 +0900

    실전 앱 만들기 4-10. 모바일 페이지 앱 만들기
    지금까지 배운 기능을 응용해, 언제나 쉽게 모바일 사이트로 이동할 수 있는 모바일 페이지 앱을 만들어본다.
    앱의 리스트에 앱 아이콘을 구현, 인트로 페이지가 존재하고 일정 시간이 지난 후 메인으로 이동한다.
    상단에는 웹사이트 바로가기 버튼과 하단에는 웹 페이지 이동, 새로고침 버튼이 있다.
    교통정보 사이트, 취업정보 사이트, 날씨 정보, 구글 지도 등의 사이트로 이동 가능하다.
    
    1. 인트로 화면
    인트로 화면이 존재하므로 Mainfest로 이동하여 시작 액티비티를 Intro로 설정한다.
    Handler(Looper.getMainLooper()).postDelayed 메소드를 통해 5초간 딜레이, intent 객체를 통해
    MainActivity로 이동하면서 Intro Activity를 finish하는 코드를 작성한다.
    Intro.xml에서 인트로 화면을 만들어준다. 앱 이름 '유용한 사이트'를 텍스트뷰로 작성한다.
    텍스트 크기는 34sp, color는 white로 지정하였다.
    
    2. Main 화면
    xml에서는 horizontal LinearLayout으로 위쪽 버튼 4개, 아래쪽 버튼 3개를 감싸고 가운데는 웹 뷰를 놓는다.
    전체 레이아웃은 LinearLayout - vertical로 하여 세로로 쌓이도록 한다.
    버튼끼리의 가중치를 1로 하여 버튼 크기를 동일하게 설정하였다.
    또한 버튼들의 레이아웃은 가중치 1, 웹 뷰의 가중치는 9로 지정한다.
    또한 vertical 안의 2개의 Linear와 1개의 웹 뷰의 layout_height를 wrap content로 하여,
    각 크기에 맞는 높이로 설정되게끔 하였다.
    
    책에서는 vertical의 layout-height와 width를 fill_parent로 하였다. match parent와 같은 기능이다.
    그리고 웹 뷰를 감싸는 LinearLayout horizontal을 따로 만들고 해당 레이아웃의 가중치를 5로 하였다.
    웹 뷰의 너비는 horizontal의 너비에 맞게 match parent, 높이는 wrap content로 지정하였다.
    또한 각 버튼 뒤의 레이아웃의 색깔도 따로 지정하였다. 나머지 구성요소는 같다.
    
    MainActivity.kt에서는 뷰 바인딩으로 activity_main.xml의 구성요소를 가져온다.
    web으로 웹 뷰를 가져오고 초기 페이지를 loadurl 메소드로 구글로 지정한다.
    또한 web.settings.javaScriptEnabled를 true로 활성화하여 자바스크립트 실행을 허용한다.
    버튼 4개에 해당하는 사이트 url을 loadurl 메소드로 클릭 이벤트를 처리한다.
    버튼 3개에 해당하는 동작을 goBack, reload, goForward 메소드로 클릭 이벤트를 처리한다.
    
    AndroidManifests.xml로 들어가 <application ... /> 위쪽 영역으로 이동하여,
    <uses-permission android:name="android.permission.INTERNET" /> 을 추가하여,
    추가된 웹뷰에 대한 인터넷 연결 권한을 설정한다.
    
    인트로가 실행되고 교통정보 사이트 접속을 제외한 나머지 사이트의 작동, 동작을 확인하여 4-10 완료.

--- app/src/main/java/com/example/kotlin_ex/Intro.kt
+++ app/src/main/java/com/example/kotlin_ex/Intro.kt
@@ -5,17 +5,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Looper
 import android.os.Handler
+import androidx.core.os.postDelayed
 
 class Intro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
 
+        // Handler - postDelayed 메소드로 5초간 인트로화면 노출 후 메인 이동
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this,MainActivity::class.java)
             startActivity(intent)
             finish()
-        },5000)
+        }, 5000)
     }
 
 
--- app/src/main/java/com/example/kotlin_ex/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_ex/MainActivity.kt
@@ -20,8 +20,7 @@ import java.util.zip.Inflater
 class MainActivity : AppCompatActivity() {
 
     // 뷰 바인딩
-    private lateinit var mBinding : ActivityMainBinding
-
+    private lateinit var mBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
@@ -30,18 +29,52 @@ class MainActivity : AppCompatActivity() {
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
 
-        var leftmovebutton = mBinding.button // 버튼이 왼쪽으로 이동
-        var bigbutton = mBinding.button2 // 누른 버튼이 커짐
+        // 웹 뷰 - 시작 페이지는 구글
+        var web = mBinding.WebView
+        web.webViewClient = WebViewClient()
+        web.loadUrl("https://www.google.com/")
+        web.settings.javaScriptEnabled = true // 자바스크립트 허용
 
-        leftmovebutton.setOnClickListener {
-            val anim = AnimationUtils.loadAnimation(this, R.anim.act1)
-            leftmovebutton.startAnimation(anim)
+        // 교통정보 버튼
+        var GYO = mBinding.button
+        GYO.setOnClickListener {
+            web.loadUrl("https://topis.seoul.go.kr/") // 서울 교통 정보 사이트
+        }
+        
+        // 취업 버튼
+        var CHUI = mBinding.button2
+        CHUI.setOnClickListener {
+            web.loadUrl("https://m.work.go.kr/main.do") // 노동부 취업 사이트 워크넷 모바일 사이트
+        }
+        
+        // 날씨 버튼
+        var NAL = mBinding.button3
+        NAL.setOnClickListener {
+            web.loadUrl("https://www.weather.go.kr/w/index.do") // 기상청 사이트
+        }
+        
+        // 지도 버튼
+        var ZIDO = mBinding.button4
+        ZIDO.setOnClickListener {
+            web.loadUrl("https://www.google.co.kr/maps/") // 구글 지도 사이트
         }
 
-        bigbutton.setOnClickListener {
-            val anim2 = AnimationUtils.loadAnimation(this, R.anim.act2)
-            bigbutton.startAnimation(anim2)
+        // BACK 버튼
+        var BACKBUTTON = mBinding.button5
+        BACKBUTTON.setOnClickListener {
+            web.goBack()
         }
 
+        // RELOAD 버튼
+        var RELOADBUTTON = mBinding.button6
+        RELOADBUTTON.setOnClickListener {
+            web.reload()
+        }
+
+        // GO 버튼
+        var GOBUTTON = mBinding.button7
+        GOBUTTON.setOnClickListener {
+            web.goForward()
+        }
     }
 }
