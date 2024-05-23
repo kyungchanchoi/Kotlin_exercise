176531c7dc6d3ce7f9a5dcc35ac81df89ef96f32
KyungChan <philip9504@naver.com>
2024-05-23 10:46:39 +0900

    실전 앱 만들기 4-9. 애니메이션 효과 주기
    우선은 책에서는 각 챕터마다 새로운 빈 프로젝트를 생성하여 진행하기 때문에,
    책과는 다르게 기존 프로젝트에서 코드를 갱신하는 방식으로 진행하는 나는,
    이전 4-8의 인트로 액티비티를 Manifest에서 첫 실행 액티비티로 실행한 것을 Main으로 수정하고 시작하였다.
    
    MainActivity에 뷰 바인딩으로 2개의 버튼을 받아온다.
    애니메이션 기능에 대한 코드를 넣어줄 anim 폴더를 생성하기 위해 res에서 Directory를 생성,
    anim라는 폴더를 생성한 후 폴더 내에 Layoutt XML File을 선택하여 act1.xml을 생성한다.
    그리고 act1.xml을 복사 붙여넣기 하여 act2.xml을 생성한다.
    생성된 xml 파일들이 현재 layout 폴더에 있는데 이를 anim 폴더로 옮겨준다.
    
    act1.xml의 코드를 작성한다. <translate 로 시작하여 />로 닫는 형태의 이동 애니메이션을 정의한다.
    android:fromXDelta - 애니메이션의 시작 위치 지정, x좌표가 0%에서 시작.
    android:toXDelta - 애니메이션의 끝 위치 지정, -100%로 입력하였으므로 왼쪽으로 이동하게 된다.
    android:duration - 애니메이션의 지속 시간, 단위가 ms이므로 10000ms는 10초.
    android:repeatCount - 애니메이션의 반복 횟수, -1은 무한 반복.
    android:fillAfter - 애니메이션 종료 후 위치를 유지할 지 여부를 지정, true시 위치를 유지.
    
    act2.xml의 코드도 작성한다. <scale 로 시작하여 /> 로 닫는 형태의 크기 조절 애니메이션을 정의한다.
    android:duration - 애니메이션의 지속 시간, 3000ms는 3초.
    android:pivotX - 애니메이션의 X축 기준점 지정, 10%면 오른쪽아래로, 100%면 왼쪽 위로 커진다.
    android:pivotY - 애니메이션의 Y축 기준점 지정.
    android:fromXScale - 애니메이션의 시작 X축 크기 비율 지정, X축 크기가 1.0배로 시작.
    android:fromYScale - 애니메이션의 시작 Y축 크기 비율 지정, Y축 크기가 1.0배로 시작.
    android:toXScale - 애니메이션의 종료 X축 크기 비율 지정, X축 크기가 3.0배로 커짐.
    android:toYScale - 애니메이션의 종료 Y축 크기 비율 지정, Y축 크기가 3.0배로 커짐.
    
    xml 코드 작성 완료 후 MainActivity로 돌아와 버튼 클릭시 애니메이션이 실행되는 코드를 작성한다.
    val anim = AnimationUtils.loadAnimation(현재 컨텍스트, 경로) 메소드를 통해,
    애니메이션 리소스 파일 act1.xml을 인수로 전달한 Animation 객체를 생성하여 anim에 할당한다.
    val anim2 변수에도 act2.xml을 인수로 전달한 Animation 객체를 생성하고 할당한다.
    
    버튼1이 왼쪽으로 이동하는 것, 버튼2가 3배로 커지는 것을 확인하여 4-9 완료.

--- app/src/main/java/com/example/kotlin_ex/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_ex/MainActivity.kt
@@ -10,6 +10,7 @@ import android.media.SoundPool
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Toast
@@ -19,14 +20,28 @@ import java.util.zip.Inflater
 class MainActivity : AppCompatActivity() {
 
     // 뷰 바인딩
-
+    private lateinit var mBinding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
 
+        // ActivityMainBinding 으로 뷰 바인딩
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
 
-    // ActivityMainBinding 으로 뷰 바인딩
+        var leftmovebutton = mBinding.button // 버튼이 왼쪽으로 이동
+        var bigbutton = mBinding.button2 // 누른 버튼이 커짐
+
+        leftmovebutton.setOnClickListener {
+            val anim = AnimationUtils.loadAnimation(this, R.anim.act1)
+            leftmovebutton.startAnimation(anim)
+        }
+
+        bigbutton.setOnClickListener {
+            val anim2 = AnimationUtils.loadAnimation(this, R.anim.act2)
+            bigbutton.startAnimation(anim2)
+        }
 
     }
 }
